a4b243bee7db8b37dc3d35bb778c669693a70a2a
Hardi SUputra <jiksdi12@gmail.com>
2019-09-23 15:04:45 +0800

    Adding test mocha and chai

---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+var expect = require("chai").expect;
+var starWars = require("./index");
+
+describe("beginner-starwars-search", function() {
+  it("should work!", function() {
+    expect(true).to.be.true;
+  });
+});
