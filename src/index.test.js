ce1efcd626cf827cf2e50987bc108a31158c3c07
Hardi SUputra <jiksdi12@gmail.com>
2019-09-24 08:46:45 +0800

    -a

--- src/index.test.js
+++ src/index.test.js
@@ -2,7 +2,26 @@ var expect = require("chai").expect;
 var starWars = require("./index");
 
 describe("beginner-starwars-search", function() {
-  it("should work!", function() {
-    expect(true).to.be.true;
+  describe("all", function() {
+    it("It's should be an array of string!", function() {
+      expect(starWars.all).to.satisfies(isArrayOfStrings);
+
+      function isArrayOfStrings(args) {
+        return args.every(function(item) {
+          return typeof item === "string";
+        });
+      }
+    });
+
+    it("Should contain `Luke Skywalker", function() {
+      expect(starWars.all).to.include("Luke Skywalker");
+    });
+  });
+
+  describe("random", function() {
+    it("Should return random item from the starWars.all", function() {
+      var randomItem = starWars.random();
+      expect(starWars.all).to.include(randomItem);
+    });
   });
 });
