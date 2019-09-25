c8829b336557637c3625ea7308a53a71dc9b9801
Hardi SUputra <jiksdi12@gmail.com>
2019-09-25 08:44:19 +0800

    fix(travis): fix travis build failed
    closes #3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 var uniqueRandomArray = require("unique-random-array");
 var starWarsnames = require("./starwars-names.json");
+var getRandomItem = uniqueRandomArray(starWarsnames);
 
 module.exports = {
   all: starWarsnames,
-  random: uniqueRandomArray(starWarsnames)
+  random: random
 };
+
+function random(number) {
+  if (number) {
+    var randomItems = [];
+    for (let i = 0; i < number; i++) {
+      randomItems.push(getRandomItem());
+    }
+    return randomItems;
+  }
+  return getRandomItem();
+}
